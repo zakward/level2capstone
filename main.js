1ce90083bc783df0c6496e71eb4c8aa5945db584
zakward85 <zakward85@gmail.com>
2023-08-25 13:24:22 -0500

    functionality

--- main.js
+++ main.js
@@ -1,1 +1,166 @@
-console.log("test")
+const form = document.form;
+
+function getData() {
+  axios
+    .get("https://api.vschool.io/zakward/todo/")
+    .then((res) => listData(res.data))
+    .catch((err) => console.log(err));
+}
+
+function clearListData() {
+  const list = document.getElementById("list");
+  while (list.firstChild) {
+    list.removeChild(list.firstChild);
+  }
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const newTodo = {
+    title: form.title.value,
+    description: form.description.value,
+    price: form.price.value,
+    imgUrl: form.imgUrl.value,
+  };
+  form.title.value = "";
+  form.description.value = "";
+  form.price.value = "";
+  form.imgUrl.value = "";
+
+  axios
+    .post("https://api.vschool.io/zakward/todo", newTodo)
+    .then((res) => getData())
+    .catch((err) => console.log(err));
+});
+
+function listData(data) {
+  clearListData();
+
+  const list = document.getElementById("list");
+  for (let i = 0; i < data.length; i++) {
+    const iD = data[i]._id;
+
+    const div = document.createElement("div");
+
+    list.appendChild(div);
+    const h1 = document.createElement("h1");
+    h1.textContent = data[i].title;
+    div.appendChild(h1);
+
+    const p = document.createElement("p");
+    p.textContent = data[i].description;
+    div.appendChild(p);
+
+    const p2 = document.createElement("p");
+    p2.textContent = data[i].price;
+    div.appendChild(p2);
+
+    const img = document.createElement("img");
+    img.src = data[i].imgUrl;
+    div.appendChild(img);
+
+    const button = document.createElement("button");
+    button.textContent = "Delete";
+    div.appendChild(button);
+
+    const button2 = document.createElement("button");
+    button2.textContent = "Edit";
+    div.appendChild(button2);
+
+    const label = document.createElement("label");
+    label.textContent = "Completed";
+    div.appendChild(label);
+
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    div.appendChild(checkbox);
+
+    if (data[i].completed === true) {
+      h1.style.textDecoration = "line-through";
+      p.style.textDecoration = "line-through";
+      p2.style.textDecoration = "line-through";
+      checkbox.checked = true;
+    }
+
+    checkbox.addEventListener("change", function () {
+      if (checkbox.checked) {
+        axios
+          .put(`https://api.vschool.io/zakward/todo/${iD}`, { completed: true })
+          .then((res) => getData())
+          .catch((err) => console.log(err));
+      } else {
+        axios
+          .put(`https://api.vschool.io/zakward/todo/${iD}`, {
+            completed: false,
+          })
+          .then((res) => getData())
+          .catch((err) => console.log(err));
+      }
+    });
+
+    button.addEventListener("click", () => {
+      axios
+        .delete(`https://api.vschool.io/zakward/todo/${iD}`)
+        .then((res) => getData())
+        .catch((err) => console.log(err));
+    });
+
+    button2.addEventListener("click", () => {
+      h1.remove(this);
+      p.remove(this);
+      p2.remove(this);
+      img.remove(this);
+
+      const imgEdit = document.createElement("input");
+      imgEdit.value = img.src;
+      div.prepend(imgEdit);
+      imgEdit.setAttribute("class", "editInput");
+
+      const descriptionEdit = document.createElement("input");
+      descriptionEdit.value = p.textContent;
+      div.prepend(descriptionEdit);
+      descriptionEdit.setAttribute("class", "editInput");
+
+      const priceEdit = document.createElement("input");
+      priceEdit.value = p2.textContent;
+      div.prepend(priceEdit);
+      priceEdit.setAttribute("type", "number");
+
+      const titleEdit = document.createElement("input");
+      titleEdit.value = h1.textContent;
+      div.prepend(titleEdit);
+      titleEdit.setAttribute("class", "editInput");
+
+      button2.remove(this);
+      const saveBtn = document.createElement("button");
+      saveBtn.textContent = "Save";
+      div.appendChild(saveBtn);
+
+      saveBtn.addEventListener("click", function () {
+        const updatedInfo = {
+          title: titleEdit.value,
+          description: descriptionEdit.value,
+          price: priceEdit.value,
+          imgUrl: imgEdit.value,
+        };
+
+        console.log(updatedInfo);
+
+        axios
+          .put(`https://api.vschool.io/zakward/todo/${iD}`, updatedInfo)
+          .then((response) => getData())
+          .catch((err) => console.log(err));
+
+        priceEdit.remove(this);
+        imgEdit.remove(this);
+        descriptionEdit.remove(this);
+        titleEdit.remove(this);
+
+        saveBtn.remove(this);
+        div.appendChild(button2);
+      });
+    });
+  }
+}
+
+getData();
